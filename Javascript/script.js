f8ed02e13d9491cd177eba839a628bafb9053c4b
naberius616 <84759195+naberius616@users.noreply.github.com>
2021-06-21 16:26:31 -0300

    Update script.js

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,47 +1,47 @@
-//alert("aloooooooo")
-//document.getElementById('nome').value = 'Olá'
+alert("Olá, somente um teste")
+document.getElementById('nome').value = 'Olá'
 
-//string
-//var nome = "Curso de JavaScript"
-//var idade = 27
-//var nome = prompt('Digite o seu nome: '); //criar input
-//var idade = prompt('Digite sua idade: '); //criar input
+string
+var nome = "Curso de JavaScript"
+var idade = 27
+var nome = prompt('Digite o seu nome: '); //criar input
+var idade = prompt('Digite sua idade: '); //criar input
 
-//number
-//var numeroInteiro = -7
-//var numeroFracionado = 10.75
 
-//boolean
-//var verdade = true
-//var falso = false
+var numeroInteiro = -7
+var numeroFracionado = 10.75
 
-//alert(numeroFracionado) //abrir um dialog
-//document.write(nome) //tipo echo na tela
-//console.log(numeroInteiro) exibir no console do inspect element
+boolean
+var verdade = true
+var falso = false
 
-//Concatenação
-//document.write("<h1>Olá José tudo de boas?</h1>")
+alert(numeroFracionado) //abrir um dialog
+document.write(nome) //tipo echo na tela
+console.log(numeroInteiro) exibir no console do inspect element
 
-//document.write("<h1>Olá " + nome + ", cabeça de disgraça, acabei de ver que você tem " + idade + " anos de idade</h1>");
+Concatenação
+document.write("<h1>Olá José tudo de boas?</h1>")
 
-//null
-//var teste = null
-//undefined
-//var teste2 = undefined
+document.write("<h1>Olá " + nome + ", cabeça de disgraça, acabei de ver que você tem " + idade + " anos de idade</h1>");
 
-//var a = 10
-//document.write( "A variável a é : " + a + "<br/>" )
-//var b = 20
-//document.write(" A variável b é : " + b + "<br/>")
-//var c = null
+null
+var teste = null
+undefined
+var teste2 = undefined
+
+var a = 10
+document.write( "A variável a é : " + a + "<br/>" )
+var b = 20
+document.write(" A variável b é : " + b + "<br/>")
+var c = null
     
-/*c = b
+c = b
 b = a
 a = c
 c = null;
 
 document.write('Suas novas variáveis: A = '+ a +' B = ' + b + ' C = ' + c);
-*/
+
 
 
 
