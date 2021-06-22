a379b610a1bcf0401f069b978adecef3eb4b2c28
naberius616 <84759195+naberius616@users.noreply.github.com>
2021-06-22 11:22:10 -0300

    Update вершина.c

--- linguagem_C/вершина.c
+++ linguagem_C/вершина.c
@@ -3,33 +3,29 @@
 
 int main (){
     int num1;
-    int elev; //nao é obrigado a declarar!//
+    int elev;
     float raiz;
     int num2;
     int result;
     char s[2] = "s";
-    char n[2] = "n";
 
-    printf("digite um número!\n");
+    printf("digite um número! \n");
     scanf("%d", &num1);
 
-    //scanf("%d", &num2);
    elev = num1 * num1;
    raiz = pow (num1, 0.5);
 
 
     if( num1 > 0){
     printf("o nuemero %d elevado ao quadrado é %d!\n", num1, elev);
-
-  
-
+    
     printf("e a raiz de %d é igual a %f \n", num1, raiz);
  
     }else{
         printf("numero inválido!");
     }
 
-    printf("Você deseja continuar? s/[ENTER]: ");
+    printf("Você deseja continuar? s/[enter]: ");
     scanf("%s", &s);
     if (s == s) {
       printf("Agora, digite um número ");
