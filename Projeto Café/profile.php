ffb7f5276a2547a0b113994e63966ab49d743f1a
naberius616 <84759195+naberius616@users.noreply.github.com>
2021-10-08 18:52:28 -0300

    Add files via upload

--- Projeto Café/profile.php
+++ Projeto Café/profile.php
@@ -5,16 +5,11 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 	<link href="/Coffe_Company/css/bootstrap.min.css" rel="stylesheet">
-	<link rel="icon" type="imagem/ico" href="https://cdn.iconscout.com/icon/free/png-512/coffee-pouch-1894903-1604920.png" />
-	<link rel="stylesheet" type="text/css" href="coffee_our.css" media="screen" />
+	<link rel="icon" type="imagem/ico" href="/Images/ico.png" />
+	<link rel="stylesheet" type="text/css" href="Profstyle.css" media="screen" />
 	<link href="iconic/font/css/open-iconic-bootstrap.css" rel="stylesheet">
 	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
-	<title>Our Coffee</title>
-	<style type="text/css">
-		body {
-			background-color: #111;
-		}
-	</style>
+	<title>Profile</title>
 </head>
 <body>
 
@@ -35,19 +30,16 @@
 	          <div class="collapse navbar-collapse" id="nav-principal">
 	            <ul class="navbar-nav ml-auto" style="">
 	              <li class="nav-item">
-	                <a href="" class="nav-link text-white menu-learn"></a>
+	                <a href="/Coffe_Company/Shop/shop.php" class="nav-link text-white menu-learn"><i class="bi bi-shop icons-position"></i>Shop</a>
 	              </li>
 	              <li class="nav-item">
-	                <a href="/Coffe_Company/Shop/shop.php" class="nav-link text-white menu-learn"><i class="bi bi-shop icons-position"></i>Shop</a>
+	                <a href="/Our_coffee/index.php" class="nav-link text-white menu-learn"><i class="bi bi-cup"></i>Our coffee</a>
 	              </li>
 	              <li class="nav-item">
 	                <a href="/Coffe_Company/About/about_us.php" class="nav-link text-white menu-learn"><i class="bi bi-building"></i>About us</a>
 	              </li>
 	              <li class="nav-item">
 	                <a href="/index.php" class="nav-link text-white menu-learn"><i class="bi bi-house-door"></i>Home</a>
-	              </li>
-	               <li class="nav-item">
-	                <a href="/profile/profile.php" class=""><img width="40px" style="margin-left: 20px;" src="/Images/profile_index.jpg"></a>
 	              </li>
 	              <li class="nav-item">
 	                <a class="nav-link text-white menu-learn" style="margin-left: 30px; margin-right: -90px;" href="/logoff.php">LOGOUT</a>
@@ -58,41 +50,31 @@
 	      </nav>
 	    </header>
     </div>
-    <!--End menu-->
-
-    <div id="progressbar"></div>
-    <div id="ScrollPath"></div>
-
-    <div class="div-black">
-    	<h1 class="text-white text-our-coffee">Our coffee:</h1>
-    	<img class="img-coffee" src="Images/beans_coffee.jpg">
+<!-- dps mudar tudin pra ficar com credencial do user-->
+    <img class="background-img" src="https://www.nvidia.com/content/dam/en-zz/Solutions/GeForce/Pascal/metatags/geforce-pascal-1070ti-hero-1280x600.jpg">
+    <div>
+    	<div class="box-informations">
+    		<img class="img-avatar" title="avatar" src="https://cdn-icons-png.flaticon.com/512/149/149071.png">
+    		<h1>User here</h1>
+    	</div>
     </div>
-
-    <div class="texts-div">
-    	<h1>Why so expensive?</h1>
-    	<img class="img_mug" src="Images/coffe_mug.jpg">
-    	<p class="lead">At first, our type of coffee was extremely expensive because it depended on the goodwill of luwaks/civets to eat the fruits. In addition, the production was done on a low scale, making the product even more expensive. Prices reached $100!
-		Now, however, farms and other spaces take these pets to expand the production of this coffee. The pets receive the fruit of coffee as food. 
-		Although production has increased, this remains an unusual and still very expensive coffee. The prices are very "salty", however, I guarantee you will not regret</p>
-		<h1>Origin</h1>
-		<img class="coffee_origin" src="Images/coffe-origin.jpg" alt="">
-		<p class="lead">Our coffee produced in the region of Italy, and became known for its somewhat peculiar production process. First, the grains are ingested by Civetas, mammals typical of Indonesia. During the digestive process, the animal releases enzymes and acids on the coffee bean, which goes through a natural fermentation process.Our coffee produced inside civeta acquires a slight taste of red fruits, with no acidity index and a slight bitter taste.</p>
+    <div class="box-informations2">
+    	<h3 class="information">About</h3>
+    	<a class="link-about" href="#">Add a few words about who you are.</a>
     </div>
-
-    <script>
-		if (document.addEventListener) {
-		    document.addEventListener("contextmenu", function(e) {
-		        e.preventDefault();
-		        return false;
-		    });
-		} else {
-		    document.attachEvent("oncontextmenu", function(e) {
-		        e = e || window.event;
-		        e.returnValue = false;
-		        return false;
-		    });
-		}
-	</script>
+    <div class="box-informations3">
+    	<h3 class="information">Ideas</h3>
+    	<h3 class="number-idea">0</h3>
+    	<h3 class="number-idea">0</h3>
+    	<h3 class="number-idea">0</h3>
+    	<h6 class="text-min">Followers</h6>
+    	<h6 class="text-min2">Following</h6>
+    	<h6 class="text-min3">Evaluations</h6>
+    </div>
+    <div class="box-informations4">
+    	<h3 class="information">Cart</h3>
+    	<p>You don't have anything in your cart,<a href="/Coffe_Company/Shop/shop.php" class="ml-2">click here</a> and start adding products to your cart</p>
+    </div>
 
     <!--Start footer-->
     <div class="Footer_box">
@@ -147,14 +129,18 @@
 	</div>
 	<!--End footer-->
 
-	<script type="text/javascript">
-		let progress = document.getElementById('progressbar');
-
-		let totalHeight = document.body.scrollHeight -window.innerHeight;
-
-		window.onscroll = function () {
-			let progressHeight = (window.pageYOffset / totalHeight) * 100;
-			progress.style.height = progressHeight + "%";
+	<script>
+		if (document.addEventListener) {
+		    document.addEventListener("contextmenu", function(e) {
+		        e.preventDefault();
+		        return false;
+		    });
+		} else {
+		    document.attachEvent("oncontextmenu", function(e) {
+		        e = e || window.event;
+		        e.returnValue = false;
+		        return false;
+		    });
 		}
 	</script>
 
